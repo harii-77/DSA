1f348ad485ed5ab2903d2c2b6753598f566dd876
Hariharan Murugan <hariharanvj7777@gmail.com>
2025-07-01 15:27:14 +0530

    day 1

--- src/main/java/org/dsa/array/Largest.java
+++ src/main/java/org/dsa/array/Largest.java
@@ -7,7 +7,32 @@ public class Largest {
     public static void main(String[] args) {
         List<Integer> nums = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
         int second = findSecondLargest(nums);
+        int third = findThirdLargest(nums);
         System.out.println(second);
+        System.out.println(third);
+    }
+
+    static int findThirdLargest(List<Integer> nums) {
+        int largest = nums.get(0);
+        int secondlargest = Integer.MIN_VALUE;
+        int thirdlargest = Integer.MIN_VALUE;
+
+        for(int i =1; i<nums.size(); i++){
+            if(largest < nums.get(i)){
+                thirdlargest = secondlargest;
+                secondlargest = largest;
+                largest = nums.get(i);
+            }
+            else if (largest > nums.get(i) && secondlargest < nums.get(i)) {
+                thirdlargest = secondlargest;
+                secondlargest = nums.get(i);
+            }
+            else if (largest > nums.get(i) && secondlargest > nums.get(i) && nums.get(i) > thirdlargest) {
+                thirdlargest = nums.get(i);
+            }
+        }
+
+        return thirdlargest;
     }
 
     static int findSecondLargest(List<Integer> nums) {
